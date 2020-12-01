c665b0ee70ff806e946a1a0add9344784f4dfbe2
PopckovS <popckovM5@yandex.com>
2020-12-01 03:26:46 +0300

    Добавил решение сортировки столбцов матрицы на С++

--- mydoc/C++/solutions/15/2/main.cpp
+++ mydoc/C++/solutions/15/2/main.cpp
@@ -12,11 +12,6 @@ using namespace std;
 using namespace std;
 const int N = 4, M = 4;
 
-/** Задача №1
- * Дана целочисленная прямоугольная матрица. Определить номер первого из
- * столбцов, содержащих хотя бы один нулевой элемент.
- */
-
 /** Вывод на экран массива */
 void out(int mas[N][M])
 {
@@ -39,6 +34,69 @@ void random(int mas[N][M])
         for (int j(0); j<M; j++)
             mas[i][j] = rand()%10+(-5);
 }
+
+
+/** Сортируем матрицу по убыванию эллементов столбцов */
+void BubbleSort(int mas[N][M])
+{
+    int temp;
+//    int i,j;
+    int r=0, s=0;
+
+    cout << "==============================" << endl;
+    for(int i=0; i<M; i++)
+    {
+        for(int j=0; j<N; j++)
+        {
+            for(int m=0; m<(M-1); m++)
+            {
+                if(mas[m][i] < mas[m+1][i])
+                {
+                    temp = mas[m][i];
+                    mas[m][i] = mas[m+1][i];
+                    mas[m+1][i] = temp;
+                }
+            }
+        }
+        cout << "======" << endl;
+    }
+
+
+    cout << "Выводим отсортированную матрицу" << endl;
+    out(mas);
+}
+
+
+
+/** Главная функция для запуска программы */
+int main()
+{
+    srand(time(NULL));
+
+    int mas[N][M];
+    random(mas);
+    out(mas);
+
+    BubbleSort(mas);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ///** Сортируем матрицу по убыванию эллементов столбцов */
@@ -71,56 +129,3 @@ void random(int mas[N][M])
 //    cout << "Выводим отсортированную матрицу" << endl;
 //    out(mas);
 //}
-
-
-
-
-/** Сортируем матрицу по убыванию эллементов столбцов */
-void BubbleSort(int mas[N][M])
-{
-    int temp;
-    int i,j;
-
-    for(int k = 0; k<N; k++)
-    {
-        for(i=0;i<M;i++)
-        {
-            for(j=0; j<(M-1); j++)
-            {
-                if(mas[j][k] < mas[j+1][k])
-                {
-                    temp = mas[j+1][k];
-                    mas[j][k] = mas[j+1][k];
-                    mas[j+1][k] = temp;
-                };
-//                if(mas[k][j]<mas[k][j+1])
-//                {
-//                    temp = mas[k][j];
-//                    mas[k][j] = mas[k][j+1];
-//                    mas[k][j+1] = temp;
-//                };
-            };
-        };
-    };
-
-    cout << "Выводим отсортированную матрицу" << endl;
-    out(mas);
-}
-
-
-
-/** Главная функция для запуска программы */
-int main()
-{
-//    setlocale(LC_ALL, "Russian");
-    srand(time(NULL));
-
-    int mas[N][M];
-    random(mas);
-    out(mas);
-//    one(mas);
-//    BubbleSort(mas);
-    BubbleSort(mas);
-//    system("pause>NUL");
-    return 0;
-}
