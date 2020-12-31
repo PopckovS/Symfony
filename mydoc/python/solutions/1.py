c1e4df2c3ca153454ac9af55348e7425362f53ee
PopkovS <popckov5@yandex.ru>
2020-12-31 06:48:52 +0300

    Добавил уроки по Алгоритмам.

---
+++ mydoc/python/solutions/1.py
@@ -0,0 +1,10 @@
+
+def summ(array):
+    if len(array) == 1:
+        return array.pop(0)
+    else:
+        return array.pop(0) + summ(array)
+
+array = [2, 4, 6]
+result = summ(array)
+print(result)
